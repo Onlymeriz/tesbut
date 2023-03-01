c5cc17fd06ab5fefb0747298ecdf53a583cb00c5
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-01 11:11:21 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -17,23 +17,24 @@ def create_env_file(session_name: str):
 
 
 # Fungsi yang akan dijalankan ketika pengguna memasukkan nama file .env dan session name
-@bot.on_message(filters.private & filters.text)
-def handle_message(bot: Client, message: Message):
-    if message.text.startswith('/start'):
-        bot.send_message(
-            chat_id=message.chat.id,
-            text='Hai! Silakan masukkan nama file .env yang ingin kamu buat.'
-        )
-    elif message.text.endswith('.env'):
-        session_name = input('Silakan masukkan session name yang ingin kamu gunakan: ')
-        create_env_file(session_name)
-        load_dotenv('.env')
-        bot.send_message(
-            chat_id=message.chat.id,
-            text=f'File .env dengan nama {message.text} sudah berhasil dibuat dan di-load. '
-                 f'String session kamu adalah {session_name}.'
-        )
+@bot.on_messa@app.on_message(filters.command(["create_env"]))
+async def create_env_file(client, message):
+    # Ambil argumen nama file .env dari pesan
+    env_file_name = message.text.split(" ")[100] + ".env"
 
+    # Tanyakan kepada pengguna untuk memberikan string session
+    await message.reply("Silakan berikan string session:")
 
-# Jalankan bot
-bot.run()
+    # Tangkap string session dari pesan balasan pengguna
+    @app.on_message(filters.private & filters.text)
+    async def get_string_session(client, message):
+        # Simpan string session ke dalam file .env
+        with open(env_file_name, "w") as f:
+            f.write(f"STRING_SESSION={message.text}")
+        await message.reply(f"File {env_file_name} berhasil dibuat dan string session berhasil disimpan.")
+
+        # Hapus callback tangkap string session
+        app.remove_handler(get_string_session)
+
+# Jalankan bot Pyrogram
+app.run()
