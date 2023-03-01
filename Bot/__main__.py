9c2d0de522cbb0a79b58c6ba65723e0e4a516dbd
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-01 11:38:49 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -14,15 +14,15 @@ from pyrogram import Client, filters
 # Membuat klien Pyrogram
 
 # Definisikan perintah untuk membuat file .env
-@app.on_message(filters.command("create_env") & filters.private)
+@app.on_message(filters.command("buat") & filters.private)
 async def create_env(client, message):
     # Looping untuk membuat file .env1 sampai .env100
     for i in range(1, 101):
         filename = f".env{i}"
         if not os.path.isfile(filename):
-            # Jika file belum ada, maka buat file baru dan isi dengan string session
+            # Jika file belum ada, maka buat file baru dan isi dengan string session sesuai urutan file
             with open(filename, "w") as file:
-                file.write(f"SESSION={message.text.split(' ')[1]}")
+                file.write(f"SESSION=session{i}")
             # Kirim file .env ke pengguna
             await message.reply_document(filename)
             break
