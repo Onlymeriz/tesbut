0b547ce7282938bfeabc75e7d92afc1a1b4bb609
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-01 22:50:49 +0700

    update config.py

--- config.py
+++ config.py
@@ -43,3 +43,4 @@ API_ID = int(getenv("API_ID", "22156937"))
 API_HASH = getenv("API_HASH", "0f8f66b06b1c53b9263bcfb1123e9c85")
 
 BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_URL = getenv("MONGO_URL", "")
