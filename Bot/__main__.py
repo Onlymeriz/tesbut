2570ad16d5bd0c3c33c765a8c3bc947f9d3abaaf
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-01 22:49:42 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from Bot import *
+from Bot.database.exdb import *
 
 # Fungsi untuk membuat file .env dan mengisi nama session di dalamnya
 
@@ -15,17 +16,10 @@ from pyrogram import Client, filters
 
 # Definisikan perintah untuk membuat file .env
 @app.on_message(filters.command("buat") & filters.private)
-async def create_env(client, message):
-    # Looping untuk membuat file .env1 sampai .env100
-    for i in range(1, 101):
-        filename = f".env{i}"
-        if not os.path.isfile(filename):
-            # Jika file belum ada, maka buat file baru dan isi dengan string session sesuai urutan file
-            with open(filename, "w") as file:
-                file.write(f"SESSION{i}={message.text.split()[1]}")
-            # Kirim file .env ke pengguna
-            await message.reply_text(f"`Sukses Dikirim dengan nama `{filename}")
-            break
+async def create_new_env(client, message):
+    session_string = f"SESSION={message.text.split()[1]}"
+    env_filename = create_env_file(session_string)
+    await message.reply_text(f"`Sukses Dikirim dengan nama `{env_filename}")
 
 # Jalankan bot
 app.run()
