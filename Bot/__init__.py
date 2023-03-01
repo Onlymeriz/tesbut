f4a468a7cd2ddd9711533cca4bd9dad5b8f22381
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-01 11:01:43 +0700

    update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -6,6 +6,7 @@ API_HASH="1a42e816cae8d86e71a4c466bba19b8c"
 BOT_TOKEN="6279149779:AAEFnmoUUcxfMeceZQJ4_E3lETYlaqWjciU"
 
 bot = Client(
+  name="app",
   api_id=API_ID,
   api_hash=API_HASH,
   bot_token=BOT_TOKEN,
