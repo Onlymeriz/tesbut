e16ecd4264d795713643fd1760a146dd26bb654e
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-01 11:14:16 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -5,22 +5,13 @@ from pyrogram.types import Message
 from Bot import *
 
 # Fungsi untuk membuat file .env dan mengisi nama session di dalamnya
-def create_env_file(session_name: str):
-    with open('.env', 'w') as f:
-        f.write(f"API_ID={os.environ.get('API_ID')}\n")
-        f.write(f"API_HASH={os.environ.get('API_HASH')}\n")
-        f.write(f"BOT_TOKEN={os.environ.get('BOT_TOKEN')}\n")
-        f.write(f"SESSION_NAME={session_name}")
-
-
-# Membuat instance Pyrogram Client
 
 
 # Fungsi yang akan dijalankan ketika pengguna memasukkan nama file .env dan session name
 @app.on_message(filters.command(["create_env"]))
 async def create_env_file(client, message):
     # Ambil argumen nama file .env dari pesan
-    env_file_name = message.text.split(" ")[100] + ".env"
+    env_file_name = message.text.split(" ")[1] + ".env"
 
     # Tanyakan kepada pengguna untuk memberikan string session
     await message.reply("Silakan berikan string session:")
