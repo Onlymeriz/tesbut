5b5b0bd3c4cf21e3092c42b7167c362ec9bed326
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-01 11:10:52 +0700

    update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,13 +1,17 @@
 from config import *
 from pyrogram import Client
+import os
+import logging
 
 API_ID="1634450"
 API_HASH="1a42e816cae8d86e71a4c466bba19b8c"
 BOT_TOKEN="6279149779:AAEFnmoUUcxfMeceZQJ4_E3lETYlaqWjciU"
 
-bot = Client(
+app = Client(
   name="app",
   api_id=API_ID,
   api_hash=API_HASH,
   bot_token=BOT_TOKEN,
   )
+  
+  logging.basicConfig(level=logging.INFO)
