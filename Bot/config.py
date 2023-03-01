548e4cffe981aa9670ac2c2c06c41167c7797253
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-01 10:37:34 +0700

    update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -14,8 +14,8 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 1634450  # integer value, dont use ""
-    API_HASH = 1a42e816cae8d86e71a4c466bba19b8c
+    API_ID = "1634450"  # integer value, dont use ""
+    API_HASH = "1a42e816cae8d86e71a4c466bba19b8c"
     TOKEN = "6279149779:AAEFnmoUUcxfMeceZQJ4_E3lETYlaqWjciU"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
 
 
