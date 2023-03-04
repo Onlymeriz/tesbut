7af14d43cfc6860e6ac6c7efc5c9f93052570e34
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-05 05:38:15 +0700

    update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -13,6 +13,38 @@ BOT_TOKEN="6279149779:AAEFnmoUUcxfMeceZQJ4_E3lETYlaqWjciU"
 
 LOOP = asyncio.get_event_loop()
 
+import logging
+from logging.handlers import RotatingFileHandler
+from config import *
+import sys
+LOG_FILE_NAME = "logs.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+logging.getLogger("pytgcalls").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
+logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
+logging.basicConfig(level=logging.INFO)
+
+LOGS = logging.getLogger(__name__)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+if not BOT_TOKEN:
+   LOGGER(__name__).error("WARNING: BOT TOKEN TIDAK DITEMUKAN, SHUTDOWN BOT")
+   sys.exit()
+
 app = Client(
   name="app",
   api_id=API_ID,
