1ecedc442f81c46153db62dc5f154b601ccec5a3
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-05 04:41:59 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -10,73 +10,29 @@ from pyrogram.errors import SessionPasswordNeeded
 from pyrogram.session import Session
 
 
-def fix_phone_number(phone_number):
-    # Menghilangkan karakter selain digit
-    phone_number = "".join(filter(str.isdigit, phone_number))
+import os
 
-    # Menghilangkan angka 0 pada awal nomor telepon
-    if phone_number.startswith("0"):
-        phone_number = phone_number[1:]
+@app.on_message(
+    filters.private & filters.command("buat") & filters.user(ADMINS) & ~filters.via_bot
+)
+async def create_env(client, message):
+    session_index = 1
+    while True:
+        filename = f".env{session_index}"
+        if os.path.isfile(filename):
+            with open(filename, "r") as file:
+                contents = file.read()
+                if message.text.split()[1] in contents:
+                    await message.reply_text(f"Session sudah tersimpan pada {filename}.")
+                    return
+            session_index += 1
+        else:
+            with open(filename, "w") as file:
+                file.write(f"SESSION{session_index}={message.text.split()[1]}")
+                load_dotenv()
+            await message.reply_text(f"Session berhasil disimpan pada {filename} dengan Posisi SESSION{session_index}.")
+            break
 
-    # Menambahkan awalan internasional jika tidak ada
-    if not phone_number.startswith("+"):
-        phone_number = "+62" + phone_number
-
-    return phone_number
-
-# Fungsi untuk membuat session
-def create_session(phone_number):
-    # Membuat session menggunakan nomor telepon pengguna
-    with app:
-        try:
-            app.send_code(phone_number)
-            # Meminta kode verifikasi
-            code = None
-            while code is None:
-                message = app.listen(filters.private & filters.regex(r"^\d{4,6}$"))
-                code = message.text
-
-            session = app.sign_in(phone_number, code)
-
-        # Jika dibutuhkan password tambahan
-        except SessionPasswordNeeded:
-            password = input("Akun Anda dilindungi oleh kata sandi. Silakan masukkan kata sandi: ")
-            session = app.sign_in(phone_number, password)
-
-    # Mengembalikan session
-    return session
-
-# Handler untuk menerima pesan dari pengguna
-@app.on_message(filters.private)
-async def handle_message(client, message):
-    # Jika pengguna mengirimkan nomor telepon
-    if message.contact is not None:
-        # Mengambil nomor telepon dari pesan
-        phone_number = message.contact.phone_number
-
-        # Memperbaiki nomor telepon
-        phone_number = fix_phone_number(phone_number)
-
-        # Membuat session menggunakan nomor telepon
-        session = await create_session(phone_number)
-
-        # Mengirimkan string session ke pengguna
-        await app.send_message(message.chat.id, f"String session Anda adalah: {session.stringify()}")
-
-    # Jika pengguna membalas pesan yang dikirimkan oleh bot
-    elif message.reply_to_message is not None:
-        # Mengambil kode verifikasi dari pesan
-        code = message.text.strip()
-
-        # Memperbarui kode verifikasi pada session
-        session = Session.try_load_or_create_new("session_file_name_here", lambda: None)
-        session = session.update(code)
-
-        # Menyimpan session ke file
-        session.save()
-
-        # Mengirimkan pesan berhasil memperbarui session
-        await app.send_message(message.chat.id, "Session berhasil diperbarui!")
     
 # Jalankan bot
 app.run()
