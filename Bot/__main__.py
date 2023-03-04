c85f582e9ad79a5f6e2f72dae1e4716a17eb77ec
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-04 19:31:49 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -6,26 +6,66 @@ from Bot import *
 from Bot.database.exdb import *
 import os
 from pyrogram import Client, filters
-import pyrogram
-from pyrogram import Client
+from pyrogram.errors import SessionPasswordNeeded
+from pyrogram.session import Session
 
 
-@app.on_message()
-async def start(client, message):
-    # Kirim pesan permintaan nomor telepon
-    await message.reply_text("Silakan kirim nomor telepon Anda untuk membuat string session.")
+def create_session(phone_number):
+    # Membuat session menggunakan nomor telepon pengguna
+    with app:
+        try:
+            app.send_code(phone_number)
+            # Meminta kode verifikasi
+            code = input("Masukkan kode verifikasi yang Anda terima: ")
+            session = app.sign_in(phone_number, code)
 
-@app.on_message(pyrogram.filters.regex(r"\+\d+"))
-async def generate_session(client, message):
-    # Mengambil nomor telepon dari pesan pengguna
-    phone_number = message.text
-    
-    # Membuat string session
-    with bot:
-        session = await bot.create_session_string(phone_number)
-        
-    # Kirim string session ke pengguna
-    await message.reply_text(f"Ini adalah string session Anda:\n\n{session}")
+        # Jika dibutuhkan password tambahan
+        except SessionPasswordNeeded:
+            password = input("Akun Anda dilindungi oleh kata sandi. Silakan masukkan kata sandi: ")
+            session = app.sign_in(phone_number, password)
+
+    # Mengembalikan session
+    return session
+
+# Fungsi untuk mengirimkan pesan dengan menunggu balasan dari pengguna
+async def send_message_and_get_reply(chat_id, text):
+    message = await app.send_message(chat_id, text)
+    reply = await app.listen(filters.reply & filters.text)
+    return reply.text
+
+# Handler untuk menerima pesan dari pengguna
+@app.on_message(filters.private)
+async def handle_message(client, message):
+    # Jika pengguna mengirimkan nomor telepon
+    if message.contact is not None:
+        # Mengambil nomor telepon dari pesan
+        phone_number = message.contact.phone_number
+
+        # Membuat session menggunakan nomor telepon
+        session = await create_session(phone_number)
+
+        # Mengirimkan string session ke pengguna
+        await app.send_message(message.chat.id, f"String session Anda adalah: {session.stringify()}")
+
+    # Jika pengguna membalas pesan yang dikirimkan oleh bot
+    elif message.reply_to_message is not None:
+        # Mengambil kode verifikasi dari pesan
+        code = message.text.strip()
+
+        # Memperbarui kode verifikasi pada session
+        session = Session.try_load_or_create_new("session_file_name_here", lambda: None)
+        session = session.update(code)
+
+        # Menyimpan session ke file
+        session.save()
+
+        # Mengirimkan pesan berhasil memperbarui session
+        await app.send_message(message.chat.id, "Session berhasil diperbarui!")
+
+    # Jika pengguna mengirimkan pesan lain
+    else:
+        # Mengirimkan pesan meminta nomor telepon
+        await app.send_message(message.chat.id, "Silakan kirimkan nomor telepon Anda menggunakan tombol 'Kirim Kontak' di bawah ini.", reply_markup={"keyboard": [[{"text": "Kirim Kontak", "request_contact": True}]], "resize_keyboard": True})
     
 # Jalankan bot
 app.run()
