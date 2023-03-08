fd265a44eb66b6987af3cf34f2de4dd706e061e3
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-08 18:56:50 +0700

    update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -19,13 +19,55 @@ from config import *
 import sys
 LOG_FILE_NAME = "logs.txt"
 
-
+AKTIFPERINTAH = {}
+# /start message when other users start your bot
+START_OTHER_USERS_TEXT = get_config(
+    "START_OTHER_USERS_TEXT",
+    (
+        """
+        Selamat datang di bot sesi darmi
+        """
+    )
+)
+INPUT_PHONE_NUMBER = get_config("INPUT_PHONE_NUMBER", (
+    "Silahkan kirim no tele kamu"
+))
+RECVD_PHONE_NUMBER_DBP = get_config("RECVD_PHONE_NUMBER_DBP", (
+    "Mencoba mengirim otp, silahkan cek otp"
+))
+ALREADY_REGISTERED_PHONE = get_config("ALREADY_REGISTERED_PHONE", (
+    "Nomor terverifikasi terdaftar tele gan"
+))
+CONFIRM_SENT_VIA = get_config("CONFIRM_SENT_VIA", (
+    "kode otp dikirim dari {}"
+))
+RECVD_PHONE_CODE = get_config("RECVD_PHONE_CODE", (
+    "Mencoba mengirim otp, silahkan cek otp"
+))
+NOT_REGISTERED_PHONE = get_config("NOT_REGISTERED_PHONE", (
+    "Nomor terverifikasi belum terdaftar tele gan"
+))
+PHONE_CODE_IN_VALID_ERR_TEXT = get_config(
+    "Kode otp salah su. ketik atau tekan ini /start"
+)
+TFA_CODE_IN_VALID_ERR_TEXT = get_config(
+    "Kode verif salah su. ketik atau tekan ini /start"
+)
+ACC_PROK_WITH_TFA = get_config("ACC_PROK_WITH_TFA", (
+    "Diverif 2 langkah nih, paste dibawah cuy"
+))
+SESSION_GENERATED_USING = get_config("SESSION_GENERATED_USING", (
+    "Terima kasih telah menggunakan bot ini ..."
+))
 
 app = Client(
   name="app",
   api_id=API_ID,
   api_hash=API_HASH,
   bot_token=BOT_TOKEN,
+  workers=BOT_WORKERS,
+  plugins=dict(root="Bot"),
+  in_memory=True,
   )
   
   
