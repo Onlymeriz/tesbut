ceede093cd65d9506f8952641d99fd8c6c137aaa
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-08 16:56:14 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from uvloop import install
 from pyrogram.types import Message
 from Bot import *
@@ -6,27 +8,35 @@ from Bot.database.exdb import *
 import os
 import re
 from itertools import count
-import os
-import importlib
-ADMINS = [1970636001, 951454060, 902478883, 2099942562, 2067434944, 1947740506, 1897354060, 1694909518]
-command_filter = filters.private & filters.command("buat") & filters.user(ADMINS) & ~filters.via_bot
-filename = ".env"
-if os.path.isfile(filename):
-    with open(filename, "r") as file:
-        contents = file.read()
-    session_list = re.findall(r"(SESSION\d+)=.*", contents)
-    session_index = len(session_list) + 1
-else:
-    session_index = 1
-@app.on_message(command_filter)
-async def create_env(client, message):
-    global session_index
-    with open(filename, "a") as file:
-        file.write(f"\nSESSION{session_index}={message.text.split()[1]}")
-        load_dotenv()
-    await message.reply_text(f"Session berhasil disimpan pada {filename} dengan Posisi SESSION{session_index}.")
-    session_index += 1
-    
-    
-    
+from Bot import app
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+@app.on_message(filters.command("start"))
+def start_command_handler(client: Client, message: Message):
+    client.send_message(
+        chat_id=message.chat.id,
+        text="Halo! Silakan masukkan nomor telepon Anda untuk membuat string session Pyrogram versi 2."
+    )
+
+@app.on_message(filters.regex(r"^\+\d{10,12}$"))
+def phone_number_handler(client: Client, message: Message):
+    phone_number = message.text
+    session_name = phone_number
+    try:
+        client.send_code(phone_number)
+        code = input("Masukkan kode verifikasi Anda: ")
+        user = client.sign_in(phone_number, code)
+        string_session = client.export_session_string()
+        client.send_message(
+            chat_id=message.chat.id,
+            text=f"String session Pyrogram Anda: {string_session}"
+        )
+    except Exception as e:
+        client.send_message(
+            chat_id=message.chat.id,
+            text=f"Terjadi kesalahan: {e}"
+        )
+
 app.run()
