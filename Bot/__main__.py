ef4fcd08d4e43435bd43a3cd45de40d9ae77689a
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-08 17:22:53 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -13,14 +13,16 @@ from Bot import app
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
+import re
+
 @app.on_message(filters.command("start"))
 async def start_command_handler(client: Client, message: Message):
     await client.send_message(
         chat_id=message.chat.id,
-        text="Halo! Silakan masukkan nomor telepon Anda untuk membuat string session Pyrogram versi 2."
+        text="Halo! Silakan masukkan nomor telepon Anda dengan kode negara +62 atau +23 untuk membuat string session Pyrogram versi 2."
     )
 
-@app.on_message(filters.regex(r"^\+\d{10,12}$"))
+@app.on_message(filters.regex(r"^\+(62)\d{10,12}$"))
 async def phone_number_handler(client: Client, message: Message):
     phone_number = message.text
     session_name = phone_number
@@ -38,6 +40,7 @@ async def phone_number_handler(client: Client, message: Message):
             chat_id=message.chat.id,
             text=f"Terjadi kesalahan: {e}"
         )
+
 
 
 
