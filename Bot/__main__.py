842ae41b0943c92aa3837c236c2760222222ff71
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-08 17:25:52 +0700

    update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -15,21 +15,44 @@ from pyrogram.types import Message
 
 import re
 
+import re
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 @app.on_message(filters.command("start"))
-async def start_command_handler(client: Client, message: Message):
+async def start_command_handler(client, message):
     await client.send_message(
         chat_id=message.chat.id,
         text="Halo! Silakan masukkan nomor telepon Anda dengan kode negara +62 atau +23 untuk membuat string session Pyrogram versi 2."
     )
 
-@app.on_message(filters.regex(r"^\+(62)\d{10,12}$"))
-async def phone_number_handler(client: Client, message: Message):
+@app.on_message(filters.regex(r"^\+(62|23)\d{10,12}$"))
+async def phone_number_handler(client, message):
     phone_number = message.text
     session_name = phone_number
     try:
         await client.send_code(phone_number)
+        keyboard = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Masukkan kode verifikasi", callback_data='enter_code')]
+        ])
+        await client.send_message(
+            chat_id=message.chat.id,
+            text="Silakan tekan tombol untuk memasukkan kode verifikasi.",
+            reply_markup=keyboard
+        )
+    except Exception as e:
+        await client.send_message(
+            chat_id=message.chat.id,
+            text=f"Terjadi kesalahan: {e}"
+        )
+
+@app.on_callback_query(filters.regex('enter_code'))
+async def enter_code_handler(client, callback_query):
+    message = callback_query.message
+    try:
         code = input("Masukkan kode verifikasi Anda: ")
-        user = await client.sign_in(phone_number, code)
+        await client.check_code(message.chat.id, code)
+        user = await client.sign_in(message.chat.id, code)
         string_session = await client.export_session_string()
         await client.send_message(
             chat_id=message.chat.id,
@@ -40,6 +63,7 @@ async def phone_number_handler(client: Client, message: Message):
             chat_id=message.chat.id,
             text=f"Terjadi kesalahan: {e}"
         )
+
 
 
 
