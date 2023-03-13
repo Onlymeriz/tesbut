da905ff304ac24d90254f432ea328d9bdf5b6c42
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-13 19:39:19 +0700

    update __init__.py

--- Bot/__init__.py
+++ Bot/__init__.py
@@ -1,5 +1,9 @@
 
 from pyrogram import Client
+import logging
+from logging.handlers import RotatingFileHandler
+from config import *
+import sys
 
 api_id = "11432539"
 
@@ -17,4 +21,33 @@ app = Client(
     bot_token=app_password
 )
 
-app.start()
+
+LOG_FILE_NAME = "logs.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(levelname)s] - %(name)s - %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler(LOG_FILE_NAME, maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("asyncio").setLevel(logging.CRITICAL)
+logging.getLogger("pytgcalls").setLevel(logging.WARNING)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
+logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
+logging.basicConfig(level=logging.INFO)
+
+LOGS = logging.getLogger(__name__)
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
+
+if not app_password:
+   LOGGER(__name__).error("WARNING: BOT TOKEN TIDAK DITEMUKAN, SHUTDOWN BOT")
+   sys.exit()
+
+
