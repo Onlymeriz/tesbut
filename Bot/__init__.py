05863d2ba4fb5a07b746636fd3b77d7d3dd2c459
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-03-13 19:17:16 +0700

    Create

---
+++ Bot/__init__.py
@@ -0,0 +1,11 @@
+from telethon import TelegramClient
+
+api_id = "11432539"
+
+api_hash = "a0dc7b4be20d3cf304345a6aca3d31f8"
+
+session_string = "BQAhIHQASRFm9xQX_UJ2wI5M5aqTeEcr9pXjiHGhxoEYdl7OvJjwHWZJEZDr0E1KID1KRncSwj8en-K5-ebDuM2f1BaFjVZNItQn00PbS3w8MRDfu1-UOGE96Qc9-OafCmP8WDBWNwBHccKMHNkuuox_AqAQlMKtO4w78CDJyii2kyXkaXkK9Jkw5vxl-TOQHl5Etn3m7pMPjUiEsD3Ql3DnLs9-dt12CO4HxSerG5Roqt5brgJ1zBYIPBZTsbRVGCmpM1QrLRf_UlifSZLZ53jizeB-2n9QItOefspkEfVga2mI5r_lFwljM7xeQ3zj4A5GY44YXKDPSJRglqkI-SY_IabgWAAAAAB1M49qAA"
+
+app_password = "6279149779:AAEFnmoUUcxfMeceZQJ4_E3lETYlaqWjciU"
+
+app = TelegramClient(session_string, api_id, api_hash)
